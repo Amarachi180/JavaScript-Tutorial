c339952ddab054abbb8727d77f23218befe1c7e1
Amarachi180 <amaraedwina18@gmail.com>
2022-10-04 22:32:36 +0100

    javascrip

---
+++ index.js
@@ -0,0 +1,25 @@
+let myName = 'Amara';
+console.log(myName);
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+let name = 'Amara'; // String literal
+let age = 30; // Number literal
+let isApproved = false; // Boolean literal
+let firstName; // undefined
+let lastName = null // when explicitly clearing the value of the variable 
+
+let person ={
+      name:'Amara', age: 30
+      }
+    console.log(person);
+   person.name = 'John'; //dot notation
+   console.log(person);
+   console.log(person.age);
+    
+   let selection = 'name';
+    person[selection] = 'Jack'; //bracket notation
+   console.log(person.name);
+
+   let selectedColors = ['red', 'blue'];
+   console.log(selectedColors);
